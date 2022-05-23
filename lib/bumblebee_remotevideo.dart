18ce503d8246072075dbdcee764da3930424cba2
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-24 00:03:04 +0100

    commit bumble bee remote video

---
+++ lib/bumblebee_remotevideo.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app1/controls_overlay.dart';
+import 'package:video_player/video_player.dart';
+
+class BumbleBeeRemoteVideo extends StatefulWidget {
+  @override
+  _BumbleBeeRemoteVideoState createState() => _BumbleBeeRemoteVideoState();
+}
+
+class _BumbleBeeRemoteVideoState extends State<BumbleBeeRemoteVideo> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+      videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(padding: const EdgeInsets.only(top: 20.0)),
+          const Text('With remote mp4'),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true, colors: VideoProgressColors(bufferedColor: Colors.black, playedColor: Colors.white),),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
